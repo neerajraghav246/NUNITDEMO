c8ad9e34b77de8f0e57dfa88b7c97b85a7374ccf
neeraj raghav <neeraj.raghav246@hgmail.com>
2018-07-25 22:50:40 +0530

    Nunit demo project moved to repository folder

---
+++ TDDExample/TDDExample.Tests/SentenceParserTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExample.Tests
+{
+    [TestFixture]
+    public class SentenceParserTest
+    {
+        SentenceParser sentenceParser;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            sentenceParser = new SentenceParser();
+        }
+
+        [Test]
+        public void TestGetNoofWordsInString()
+        {
+            string sentence = "coding is fun";
+            // this contains three words, we expect our code to return 3
+            var noofWords = sentenceParser.GetNoofWordsInString(sentence);
+            Assert.AreEqual(3, noofWords);
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            sentenceParser = null;
+        }
+    }
+}
---
+++ TDDExample/TDDExample/SentenceParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExample
+{
+    public class SentenceParser
+    {
+        public int GetNoofWordsInString(string words)
+        {
+            int count = 1;
+            int nextspaceposition = words.IndexOf(" ");
+            while (nextspaceposition != -1 && words.Length != 0)
+            {
+                words = words.Remove(0, nextspaceposition + 1);
+                count++;
+                nextspaceposition = words.IndexOf(" ");
+            }
+            return count;
+        }
+    }
+}
